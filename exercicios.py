bc4188730b7e6617b9b1496b66b934e5165712ea
PEdro <pedro.e.bachmann@gmail.com>
2026-03-01 10:56:53 -0300

    Ex.1

--- exercicios.py
+++ exercicios.py
@@ -3,12 +3,16 @@
 #%%
 #Inteiros (int)
 #1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+numero1 = int(input("Digite um numero inteiro: "))
+numero2 = int(input("Digite um numero inteiro: "))
+soma= numero1 + numero2
+print(f"A soma dos numeros inteiro é: {soma}")
+
 #2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 #3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 #4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 #5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
-#%%
 #Números de Ponto Flutuante (float)
 #6. Escreva um programa que receba dois números flutuantes e realize sua adição.
 #7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.

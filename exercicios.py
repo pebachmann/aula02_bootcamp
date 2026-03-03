a2ed3515c26372e1fbdd7bf9eaea4697e1481712
PEdro <pedro.e.bachmann@gmail.com>
2026-03-03 05:38:02 -0300

    5 exercicios

--- exercicios.py
+++ exercicios.py
@@ -1,6 +1,5 @@
 #Exercícios
 
-#%%
 #Inteiros (int)
 #1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
 numero1 = int(input("Digite um numero inteiro: "))
@@ -9,9 +8,29 @@ soma= numero1 + numero2
 print(f"A soma dos numeros inteiro é: {soma}")
 
 #2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+numero3 = float(input("Digite um numero: "))
+divisao_inteira = numero3 // 5
+print(f"O resto da sua divisão inteira por 5 é: {divisao_inteira}")
+
 #3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+numero4 = float(input("Digite um numero: "))
+numero5 = float(input("Digite um numero: "))
+multiplicacao = numero4 * numero5
+
+print(f"A multiplicacao de {numero4} por {numero5} é: {multiplicacao}")
+
 #4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+numero6 = int(input("Digite um numero inteiro: "))
+numero7 = int(input("Digite um numero inteiro: "))
+divisao_inteira2 = numero6 // numero7
+print(f"O resto da sua divisão inteira por 5 é: {divisao_inteira}")
+
 #5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+numero8 = float(input("Digite um numero: "))
+potencia = numero8 ** 2
+print(f"A potencia do {numero8} é {potencia}")
+
 
 #Números de Ponto Flutuante (float)
 #6. Escreva um programa que receba dois números flutuantes e realize sua adição.
@@ -20,7 +39,7 @@ print(f"A soma dos numeros inteiro é: {soma}")
 #9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
 #10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
 
-#%%
+
 #Strings (str)
 #11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
 #12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
